e9328e90e003742463f6424d29e81d99709d5627
Gbanaibolou Burutolu <gbanaibolou@gmail.com>
2020-07-11 19:36:31 +0100

    Add files via upload

---
+++ precache-manifest.053a1e9b7c5f77bac29ce0345ed8e6e7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd3c2505e43d48e5e96ae222afaca08e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e97aa3b237267ab24b36",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "1ef8b955e36b7bed0ab3",
+    "url": "/static/css/main.5d47bf1c.chunk.css"
+  },
+  {
+    "revision": "e97aa3b237267ab24b36",
+    "url": "/static/js/2.43cc8741.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.43cc8741.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ef8b955e36b7bed0ab3",
+    "url": "/static/js/main.a3d663d4.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
